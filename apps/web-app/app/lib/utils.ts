91fe7396713616f0c0e332c58da4f75a1e4a96c1
wanxiankai <xiankai.wan@gmail.com>
2025-07-07 21:50:07 +0800

    feat: change lib file dir

--- apps/web-app/app/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * 合并多个类名，解决类名冲突问题
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
